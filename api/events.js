41b023f0e2d2852911c0a69f3a4106b303cdcf20
thewordtech <techarts@thewordchurches.com>
2026-07-07 16:56:07 -0500

    Create events.js

---
+++ api/events.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+  try {
+    const response = await axios.get(
+      'https://api.planningcenteronline.com/calendar/v2/events',
+      {
+        auth: {
+          username: process.env.PCO_CLIENT_ID,
+          password: process.env.PCO_SECRET
+        }
+      }
+    );
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    });
+  }
+};
