33edab5d5b9f79e0ef336d3f715f3f52c9e9afd8
thewordtech <techarts@thewordchurches.com>
2026-07-07 17:50:18 -0500

    Update events.js

--- api/events.js
+++ api/events.js
@@ -6,31 +6,24 @@ module.exports = async (req, res) => {
 
   try {
 
-    const response =*await axios.get(
-      "https://api.planningcenteronline.com/calendar/v2/events",
+    const response = await axios.get(
+      "https://api.planningcenteronline.com/calendar/v2/events?include=tags",
       {
-        auth: *
-          username: process.env.P*O_CLIENT_ID,
-          password: p*ocess.env.PCO_SECRET
+        auth: {
+          username: process.env.PCO_CLIENT_ID,
+          password: process.env.PCO_SECRET
         }
-   *  }
+      }
     );
 
-    const events = res*onse.data.data
-      .filter(event*=> event.attributes.visible_in_chu*ch_center)
-      .map(event => ({
-*       id: event.id,
-        title* event.attributes.name,
-        de*cription: event.attributes.descrip*ion
-      }));
+    res.status(200).json(response.data);
 
-    res.status(200*.json(events);
-
-  } catch (error) *
+  } catch(error) {
 
     res.status(500).json({
-     *error: error.message
+      error: error.message
     });
 
   }
-*};
+
+};
