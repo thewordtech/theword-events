9e3adcde64e82cfd1427e08c913a2b6467de2c61
thewordtech <techarts@thewordchurches.com>
2026-07-07 21:21:35 -0500

    Update events.js

--- api/events.js
+++ api/events.js
@@ -9,10 +9,8 @@ module.exports = async (req, res) => {
       password: process.env.PCO_SECRET
     };
 
-    const today = new Date().toISOString();
-
     const response = await axios.get(
-      `https://api.planningcenteronline.com/calendar/v2/event_instances?where[starts_at][gte]=${today}&per_page=1&include=event`,
+      "https://api.planningcenteronline.com/calendar/v2/events/21670164/tags",
       { auth }
     );
 
