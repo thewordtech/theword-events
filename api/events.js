bd0297479989f475db8d44807875a93b3fc2f84b
thewordtech <techarts@thewordchurches.com>
2026-07-07 20:47:48 -0500

    Update events.js

--- api/events.js
+++ api/events.js
@@ -2,215 +2,25 @@ const axios = require("axios");
 
 module.exports = async (req, res) => {
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
   try {
 
-    const auth = {
-      username: process.env.PCO_CLIENT_ID,
-      password: process.env.PCO_SECRET
-    };
-
-    // Get only featured events
-    let eventsUrl =
-      "https://api.planningcenteronline.com/calendar/v2/events?where[featured]=true";
-
-    let featuredEvents = [];
-
-    while (eventsUrl) {
-
-      const response =
-        await axios.get(eventsUrl, { auth });
-
-      featuredEvents =
-        featuredEvents.concat(
-          response.data.data
-        );
-
-      eventsUrl =
-        response.data.links.next || null;
-
-    }
-
-    // Build lookup
-    const featuredMap = {};
-
-    featuredEvents.forEach(event => {
-
-      featuredMap[event.id] = {
-
-        id: event.id,
-
-        title:
-          event.attributes.name,
-
-        image:
-          event.attributes.image_url
-            ? event.attributes.image_url.replace(/&amp;/g, "&")
-            : null,
-
-        date: null,
-        location: null,
-        url: null
-
-      };
-
-    });
-
-    const today =
-      new Date().toISOString();
-
-    let instancesUrl =
-      `https://api.planningcenteronline.com/calendar/v2/event_instances?where[starts_at][gte]=${today}&per_page=100`;
-
-    let foundCount = 0;
-
-    while (
-      instancesUrl &&
-      foundCount < featuredEvents.length
-    ) {
-
-      const response =
-        await axios.get(instancesUrl, { auth });
-
-      response.data.data.forEach(instance => {
-
-        const eventId =
-          instance.relationships.event.data.id;
-
-        if (
-          featuredMap[eventId] &&
-          !featuredMap[eventId].date
-        ) {
-
-          featuredMap[eventId].date =
-            instance.attributes.starts_at;
-
-          featuredMap[eventId].location =
-            instance.attributes.location;
-
-          featuredMap[eventId].url =
-            instance.attributes.church_center_url;
-
-          foundCount++;
-
+    const response = await axios.get(
+      "https://api.planningcenteronline.com/calendar/v2/events?where[featured]=true&per_page=5",
+      {
+        auth: {
+          username: process.env.PCO_CLIENT_ID,
+          password: process.env.PCO_SECRET
         }
+      }
+    );
 
-      });
+    res.status(200).json(response.data);
 
-      instancesUrl =
-        response.data.links.next || null;
-
-    }
-
-    const events =
-      Object.values(featuredMap)
-
-      .filter(event => event.date)
-
-      .sort(
-        (a, b) =>
-          new Date(a.date) -
-          new Date(b.date)
-      );
-
-    res.status(200).json(events);
-
-  } catch (error) {
+  } catch(error){
 
     res.status(500).json({
-      error: error.message
-    });
-
-  }
-
-};    // Build lookup by event id
-    const featuredMap = {};
-
-    featuredEvents.forEach(event => {
-
-      featuredMap[event.id] = {
-
-        id: event.id,
-
-        title: event.attributes.name,
-
-        image:
-          event.attributes.image_url
-            ? event.attributes.image_url.replace(/&amp;/g, "&")
-            : null,
-
-        date: null,
-        location: null,
-        url: null
-
-      };
-
-    });
-
-    const today =
-      new Date().toISOString();
-
-    let instancesUrl =
-      `https://api.planningcenteronline.com/calendar/v2/event_instances?where[starts_at][gte]=${today}&per_page=100`;
-
-    let foundCount = 0;
-
-    while (
-      instancesUrl &&
-      foundCount < featuredEvents.length
-    ) {
-
-      const response =
-        await axios.get(instancesUrl, { auth });
-
-      response.data.data.forEach(instance => {
-
-        const eventId =
-          instance.relationships.event.data.id;
-
-        if (
-          featuredMap[eventId] &&
-          !featuredMap[eventId].date
-        ) {
-
-          featuredMap[eventId].date =
-            instance.attributes.starts_at;
-
-          featuredMap[eventId].location =
-            instance.attributes.location;
-
-          featuredMap[eventId].url =
-            instance.attributes.church_center_url;
-
-          foundCount++;
-
-        }
-
-      });
-
-      instancesUrl =
-        response.data.links.next || null;
-
-    }
-
-    const events =
-      Object.values(featuredMap)
-
-      .filter(event => event.date)
-
-      .sort(
-        (a, b) =>
-          new Date(a.date) -
-          new Date(b.date)
-      );
-
-    res.status(200).json(events);
-
-  } catch (error) {
-
-    res.status(500).json({
-      error: error.message
+      error: error.message,
+      details: error.response?.data || null
     });
 
   }
