fa0080ef6eac8934bd6369e6258c64b09a546e5f
thewordtech <techarts@thewordchurches.com>
2026-07-07 21:55:44 -0500

    Update events.js

--- api/events.js
+++ api/events.js
@@ -5,7 +5,193 @@ module.exports = async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
   res.setHeader(
+    "Cache-Control",const axios = require("axios");
+
+module.exports = async (req, res) => {
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  res.setHeader(
     "Cache-Control",
+    "s-maxage=900, stale-while-revalidate=3600"
+  );
+
+  try {
+
+    const auth = {
+      username: process.env.PCO_CLIENT_ID,
+      password: process.env.PCO_SECRET
+    };
+
+    let eventsUrl =
+      "https://api.planningcenteronline.com/calendar/v2/events?where[featured]=true&include=tags";
+
+    let featuredEvents = [];
+
+    while (eventsUrl) {
+
+      const response =
+        await axios.get(eventsUrl, { auth });
+
+      featuredEvents =
+        featuredEvents.concat(
+          response.data.data
+        );
+
+      eventsUrl =
+        response.data.links.next || null;
+
+    }
+
+    const featuredMap = {};
+
+    featuredEvents.forEach(event => {
+
+      const tagIds =
+        event.relationships?.tags?.data?.map(
+          tag => tag.id
+        ) || [];
+
+      const campuses = [];
+
+      if (tagIds.includes("245632")) {
+        campuses.push("Lakeside");
+      }
+
+      if (tagIds.includes("245633")) {
+        campuses.push("Springtown");
+      }
+
+      if (tagIds.includes("343409")) {
+        campuses.push("Aledo");
+      }
+
+      featuredMap[event.id] = {
+
+        id: event.id,
+
+        title:
+          event.attributes.name,
+
+        campuses,
+
+        image:
+          event.attributes.image_url
+            ? event.attributes.image_url.replace(/&amp;/g, "&")
+            : null,
+
+        date: null,
+
+        endDate: null,
+
+        sessions: [],
+
+        location: null,
+
+        url: null
+
+      };
+
+    });
+
+    const today =
+      new Date().toISOString();
+
+    let instancesUrl =
+      `https://api.planningcenteronline.com/calendar/v2/event_instances?where[starts_at][gte]=${today}&per_page=100`;
+
+    let foundCount = 0;
+
+    while (
+      instancesUrl &&
+      foundCount < 12
+    ) {
+
+      const response =
+        await axios.get(
+          instancesUrl,
+          { auth }
+        );
+
+      response.data.data.forEach(instance => {
+
+        const eventId =
+          instance.relationships.event.data.id;
+
+        if (!featuredMap[eventId]) {
+          return;
+        }
+
+        featuredMap[eventId].sessions.push({
+
+          start:
+            instance.attributes.starts_at,
+
+          end:
+            instance.attributes.ends_at
+
+        });
+
+        if (!featuredMap[eventId].date) {
+
+          featuredMap[eventId].date =
+            instance.attributes.starts_at;
+
+          featuredMap[eventId].endDate =
+            instance.attributes.ends_at;
+
+          featuredMap[eventId].location =
+            instance.attributes.location;
+
+          featuredMap[eventId].url =
+            instance.attributes.church_center_url;
+
+          foundCount++;
+
+        }
+
+      });
+
+      instancesUrl =
+        response.data.links.next || null;
+
+    }
+
+    Object.values(featuredMap)
+      .forEach(event => {
+
+        event.sessions.sort(
+          (a, b) =>
+            new Date(a.start) -
+            new Date(b.start)
+        );
+
+      });
+
+    const events =
+      Object.values(featuredMap)
+
+        .filter(event => event.date)
+
+        .sort(
+          (a, b) =>
+            new Date(a.date) -
+            new Date(b.date)
+        )
+
+        .slice(0, 12);
+
+    res.status(200).json(events);
+
+  } catch (error) {
+
+    res.status(500).json({
+      error: error.message
+    });
+
+  }
+
+};
     "s-maxage=900, stale-while-revalidate=3600"
   );
 
