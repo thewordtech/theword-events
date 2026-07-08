6c701343daeeb0f8208ac2bc83d834889ba2da4c
thewordtech <techarts@thewordchurches.com>
2026-07-07 19:59:37 -0500

    Update events.js

--- api/events.js
+++ api/events.js
@@ -2,27 +2,56 @@ const axios = require("axios");
 
 module.exports = async (req, res) => {
 
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
   try {
 
-    const response = await axios.get(
-      "https://api.planningcenteronline.com/calendar/v2/event_instances?order=-starts_at&per_page=20",
-      {
+    let url =
+      "https://api.planningcenteronline.com/calendar/v2/events";
+
+    let allEvents = [];
+
+    while (url) {
+
+      const response = await axios.get(url, {
         auth: {
           username: process.env.PCO_CLIENT_ID,
           password: process.env.PCO_SECRET
         }
-      }
-    );
+      });
 
-    res.status(200).json(
-      response.data.data.map(i => ({
-        name: i.attributes.name,
-        eventId: i.relationships.event.data.id,
-        date: i.attributes.starts_at
-      }))
-    );
+      allEvents = allEvents.concat(response.data.data);
 
-  } catch(error){
+      url = response.data.links.next || null;
+
+    }
+
+    const events = allEvents
+
+      .filter(event =>
+        event.attributes.featured === true
+      )
+
+      .sort((a, b) =>
+        new Date(b.attributes.updated_at) -
+        new Date(a.attributes.updated_at)
+      )
+
+      .slice(0, 15)
+
+      .map(event => ({
+        id: event.id,
+        title: event.attributes.name,
+        summary: event.attributes.summary,
+        image: event.attributes.image_url
+          ? event.attributes.image_url.replace(/&amp;/g, "&")
+          : null,
+        url: event.links.html
+      }));
+
+    res.status(200).json(events);
+
+  } catch(error) {
 
     res.status(500).json({
       error: error.message
